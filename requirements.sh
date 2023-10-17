8a120f0eb4c09658ac33672a8d7c0f9d372b4bb0
Vincent Prins <Vincent.Prins@ivdnt.org>
2023-10-17 12:54:42 +0200

    Added --no-cache-dir

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,2 @@
 pip install lxml JSON_minify gensim tqdm numpy termcolor scikit_learn terminaltables pyyaml typing click --no-cache-dir \
-	&& pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+	&& pip install torch torchvision torchaudio --no-cache-dir --index-url https://download.pytorch.org/whl/cu121
