c9d52f899f0393811445c9ec4eb8d00c964cde10
Vincent Prins <Vincent.Prins@ivdnt.org>
2023-10-17 12:51:27 +0200

    Use pytorch GPU

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,2 @@
 pip install lxml JSON_minify gensim tqdm numpy termcolor scikit_learn terminaltables pyyaml typing click --no-cache-dir \
-	&& pip install torch --no-cache-dir --extra-index-url https://download.pytorch.org/whl/cpu
+	&& pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
