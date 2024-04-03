377f53be97c18b0fb5d9577d2ad7e3a1e8d44120
Vincent Prins <Vincent.Prins@ivdnt.org>
2024-04-03 16:53:14 +0200

    freeze requirements

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,1 @@
-pip install lxml JSON_minify gensim tqdm numpy termcolor scikit_learn terminaltables pyyaml typing click --no-cache-dir \
-	&& pip install torch torchvision torchaudio --no-cache-dir --index-url https://download.pytorch.org/whl/cu121
+pip install --no-cache-dir -r requirements.txt
